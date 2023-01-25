c62308c034dc9067ee64ad8da412992baf481f72
Nigraum <yan.corintima@hotmail.com>
2023-01-25 20:03:26 -0300

    fix: fixing some lines of the script

--- number_guess.sh
+++ number_guess.sh
@@ -5,19 +5,19 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo "Enter your username:"
 read USERNAME
 
-USERNAME_AVAIL=$($PQSL "SELECT username FROM users WHERE username='$USERNAME'")
+USERNAME_AVAIL=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
 GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
-BEST_GAME=$($PSQL "SELECT MIN(number_guesses) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")
+BEST_GAME=$($PSQL "SELECT MIN(number_guesses) FROM users INNER JOIN games USING(user_id) WHERE username = '$USERNAME'")  
 
 if [[ -z $USERNAME_AVAIL ]]
   then
     INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
     echo "Welcome, $USERNAME! It looks like this is your first time here."
   else
-  echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took <best_game> guesses.
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
-RANDOM_NUM=$((1 + $RANDOM % 1000))
+RANDOM_NUM=$(( 1 + $RANDOM % 1000))
 GUESS=1
 echo "Guess the secret number between 1 and 1000:"
 
@@ -27,7 +27,7 @@ while read NUM
       then
       echo "That is not an integer, guess again:"
       else
-      if [[ $NUM -eq $RANDOM_NUM ]]
+      if [[  $NUM -eq $RANDOM_NUM ]]
         then
         break;
         else
@@ -43,12 +43,12 @@ while read NUM
     GUESS=$(( $GUESS + 1 ))
 done
 
-if [[ $GUESS == 1 ]]
+if [[  $GUESS == 1 ]]
   then
     echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
   else
-   echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
+  echo "You guessed it in $GUESS tries. The secret number was $RANDOM_NUM. Nice job!"
 fi
 
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
-INSERT_GAME=$($PSQL "INSERT INTO games(number_guesses, user_id) VALUES($guess, $USERNAME_ID)")
+INSERT_GAME=$($PSQL "INSERT INTO games(number_guesses, user_id) VALUES($GUESS, $USER_ID)")
